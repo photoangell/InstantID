4b81e550ee45ed3ff96b6337036e709ab30f8497
Stephen Angell <photoangell@gmail.com>
2024-10-30 15:20:54 +0000

    add flask run for local testing

--- vercel-mock/api/sample_image_endpoint.py
+++ vercel-mock/api/sample_image_endpoint.py
@@ -45,3 +45,6 @@ def download_image():
         return send_file(SAMPLE_IMAGE_PATH, mimetype='image/jpeg')
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  
