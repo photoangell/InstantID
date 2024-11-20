ed09165fcda381b83058c9737f27d34aa6796aef
Stephen Angell <photoangell@gmail.com>
2024-11-20 16:18:31 +0000

    Add libraries for image pipeline

--- gradio_demo/UFC_operator.py
+++ gradio_demo/UFC_operator.py
@@ -1,4 +1,11 @@
 import gradio as gr
+import argparse
+import json
+import sys
+from pathlib import Path
+from modules.image_pipeline import initialize_pipeline, generate_image
+
+
 
 def analyze_person(input_image, reference_image, gender, race, hair_length):
     # Here you would add your image analysis logic
